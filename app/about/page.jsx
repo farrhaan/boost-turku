ad09a81f2ea4749d14d7681fc1a4fa1852d97c1f
M Farhan <m.ffarrhan@gmail.com>
2024-07-26 15:15:13 +0300

    fix: footer layout fixed

---
+++ app/about/page.jsx
@@ -0,0 +1,22 @@
+
+// import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+
+export default function AboutPage() {
+  return (
+    <div className="bg-primary-black overflow-hidden">
+      about
+
+    {/* <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
+    </div> */}
+    {/* <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+    </div> */}
+  </div>
+  )
+}
+
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World }
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
-    <Navbar />
+    {/* <Navbar /> */}
     <Hero />
     <div className="relative">
       <About />
@@ -21,7 +21,7 @@ const Page = () => (
       <div className="gradient-04 z-0" />
       <Feedback />
     </div> */}
-    <Footer />
+    {/* <Footer /> */}
   </div>
 );
 
