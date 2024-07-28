c2e4de611d20076a41eedec4558f6dfcb392ff6a
Muhammad Shahzad Ali <44517352+Shahzad6077@users.noreply.github.com>
2024-07-28 06:24:39 +0500

    :sparkles: add posts pages & sanityCMS studio

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,59 +6,64 @@ import { socials } from "../constants";
 import styles from "../styles";
 import { footerVariants } from "../utils/motion";
 import Image from "next/image";
+import ThemeSwitch from "@/components/themeSwitch";
 
 const Footer = () => (
   <motion.footer
     variants={footerVariants}
-    initial='hidden'
-    whileInView='show'
-    className={`${styles.xPaddings} py-8 relative`}>
-    <div className='footer-gradient' />
-    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-      <div className='flex items-center justify-between flex-wrap gap-5'>
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} relative py-8`}>
+    <div className="footer-gradient" />
+    <div
+      className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex flex-wrap items-center justify-between gap-5">
         <div>
-          <h4 className='font-bold md:text-[64px] text-[44px] text-white'>
+          <h4 className="text-[44px] font-bold text-white md:text-[64px]">
             Join us on Telegram
           </h4>
-          <p className='text-white'>
-            Subscribe to Boost on Telegram and be the first one to get the
-            latest news and opportunities!
+          <p className="text-white">
+            Subscribe to Boost on Telegram and be the first one to get
+            the latest news and opportunities!
           </p>
         </div>
         <a
-          href='https://t.me/boostturku'
-          className='flex items-center h-fit py-2 px-4 bg-brand-red rounded-[30px]'>
+          href="https://t.me/boostturku"
+          className="flex h-fit items-center rounded-[30px] bg-brand-red px-4 py-2">
           <img
-            src='/boost/telegram.svg'
-            alt='headset'
-            className='h-9 object-contain'
+            src="/boost/telegram.svg"
+            alt="headset"
+            className="h-9 object-contain"
           />
-          <span className='font-normal text-[16px] text-white'>Subscribe</span>
+          <span className="text-[16px] font-normal text-white">
+            Subscribe
+          </span>
         </a>
       </div>
 
-      <div className='flex flex-col'>
-        <div className='mb-[50px] h-[2px] bg-white opacity-10' />
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
-        <div className='flex items-center justify-between flex-wrap gap-4'>
+        <div className="flex flex-wrap items-center justify-between gap-4">
           <Image
-            src='/boost/boost-logo.png'
-            alt='Boost Logo'
+            src="/boost/boost-logo.png"
+            alt="Boost Logo"
             width={120}
             height={40}
           />
 
-          <p className='font-normal text-[14px] text-white opacity-50'>
+          <p className="text-[14px] font-normal text-white opacity-50">
             Copyright © 2024. All rights reserved.
           </p>
 
-          <div className='flex gap-4'>
-            {socials.map((social) => (
+          <div className="flex gap-4">
+            <ThemeSwitch />
+            {socials.map(social => (
               <a key={social.name} href={social.link}>
                 <img
                   src={social.url}
                   alt={social.name}
-                  className='w-[24px] h-[24px] object-contain'
+                  className="h-[24px] w-[24px] object-contain"
                 />
               </a>
             ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{html,js,jsx}',
-    './components/**/*.{html,js,jsx}',
-    './sections/**/*.{html,js,jsx}',
-    './styles/**/*.{js,jsx}',
+    "./app/**/*.{html,js,jsx}",
+    "./components/**/*.{html,js,jsx}",
+    "./sections/**/*.{html,js,jsx}",
+    "./styles/**/*.{js,jsx}"
   ],
-  mode: 'jit',
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        'primary-black': 'rgb(12 0 0)',
-        'secondary-white': '#c7c7c7',
-        'brand-red': "#C52127"
+        "primary-black": "rgb(12 0 0)",
+        "secondary-white": "#c7c7c7",
+        "brand-red": "#C52127"
       },
       transitionTimingFunction: {
-        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+        "out-flex": "cubic-bezier(0.05, 0.6, 0.4, 0.9)"
       },
-    },
+      aspectRatio: {
+        "4/3": "4 / 3",
+        "3/2": "3 / 2",
+        "2/3": "2 / 3",
+        "9/16": "9 / 16"
+      }
+    }
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")]
 };
